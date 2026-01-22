1f881ffa4c7cdb8cad6750972afc39c9348e0ca8
noyon-01 <shahadat.hosen.noyon.01@gmail.com>
2026-01-22 21:41:12 +0600

    javascript boolean type variable is added

--- javascript-variable/variable.js
+++ javascript-variable/variable.js
@@ -1,3 +1,4 @@
+// !JavaScript String Type Variable Starts Here! //
 // var myName = "Shahadat Hosen Noyon";
 // console.log(myName);
 // console.log(typeof myName);
@@ -5,7 +6,9 @@
 // let string = "Hello World!";
 // console.log(string);
 // console.log(typeof string);
+// !JavaScript String Type Variable Ends Here! //
 
+// !JavaScript Number Type Variable Starts Here! //
 // var penPrice = 10;
 // console.log(penPrice);
 // console.log(typeof penPrice);
@@ -13,3 +16,14 @@
 // let number = 404;
 // console.log(number);
 // console.log(typeof number);
+// !JavaScript Number Type Variable Ends Here! //
+
+// !JavaScript Boolean Type Variable Starts Here! //
+// var passed = true;
+// console.log(passed);
+// console.log(typeof passed);
+
+// let workDone = false;
+// console.log(workDone);
+// console.log(typeof workDone);
+// !JavaScript Boolean Type Variable Ends Here! //
