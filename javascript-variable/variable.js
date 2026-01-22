dc6f0b6caa8bce3f09e11a650d0e27927096fafd
noyon-01 <shahadat.hosen.noyon.01@gmail.com>
2026-01-22 21:34:30 +0600

    javascript number type variable is added

--- javascript-variable/variable.js
+++ javascript-variable/variable.js
@@ -1,7 +1,15 @@
-var myName = "Shahadat Hosen Noyon";
-console.log(myName);
-console.log(typeof myName);
+// var myName = "Shahadat Hosen Noyon";
+// console.log(myName);
+// console.log(typeof myName);
 
-let string = "Hello World!";
-console.log(string);
-console.log(typeof string);
+// let string = "Hello World!";
+// console.log(string);
+// console.log(typeof string);
+
+// var penPrice = 10;
+// console.log(penPrice);
+// console.log(typeof penPrice);
+
+// let number = 404;
+// console.log(number);
+// console.log(typeof number);
