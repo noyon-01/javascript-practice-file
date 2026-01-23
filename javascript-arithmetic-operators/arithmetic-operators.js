f8c4834a79e624d197c59a312fd8b5af79cf4ae4
noyon-01 <shahadat.hosen.noyon.01@gmail.com>
2026-01-23 16:08:28 +0600

    javascript arithmetic operators addition, subtraction, multiplication, division and rimainder operators is added

---
+++ javascript-arithmetic-operators/arithmetic-operators.js
@@ -0,0 +1,54 @@
+// !JavaScript Addition Operation Starts Here! //
+// let x = 10;
+// let y = 20;
+// let z = x + y;
+// console.log(z);
+
+// let price = 100;
+// let newPrice = (price += 50);
+// console.log(newPrice);
+// !JavaScript Addition Operation Ends Here! //
+
+// !JavaScript Subtraction Operation Starts Here! //
+// let x = 50;
+// let y = 23;
+// let z = x - y;
+// console.log(z);
+
+// let price = 1000;
+// let discountPrice = price -= 130;
+// console.log(discountPrice);
+// !JavaScript Subtraction Operation Ends Here! //
+
+// !JavaScript Multiplication Operation Starts Here! //
+// let x = 5;
+// let y = 7;
+// let z = x * y;
+// console.log(z);
+
+// let price = 10;
+// let newPrice = price *= 3;
+// console.log(newPrice);
+// !JavaScript Multiplication Operation Ends Here! //
+
+// !JavaScript Division Operation Starts Here! //
+// let x = 36;
+// let y = 6;
+// let z = x / y;
+// console.log(z);
+
+// let price = 49;
+// let newPrice = price /= 7;
+// console.log(newPrice);
+// !JavaScript Division Operation Ends Here! //
+
+// !JavaScript Remainder Operation Starts Here! //
+// let a = 23;
+// let b = 7;
+// let z = 23 % 7;
+// console.log(z);
+
+// let price = 45;
+// let discountPrice = price %= 7;
+// console.log(discountPrice);
+// !JavaScript Remainder Operation Ends Here! //
