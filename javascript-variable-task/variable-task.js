56ba91e356c855cb94f805ad3d607bb181ef151f
noyon-01 <shahadat.hosen.noyon.01@gmail.com>
2026-01-25 18:47:19 +0600

    javascript variable practice task is added

---
+++ javascript-variable-task/variable-task.js
@@ -0,0 +1,36 @@
+// !JavaScript Variable Practice Task - 01! //
+// let orange = 100;
+// let apple = 200;
+
+// let myAmount = 1000;
+// let totalAmount = orange + apple;
+// let returnAmount = myAmount - totalAmount;
+// console.log(`My Amount is ${myAmount} taka.`);
+// console.log(`1kg Orange and Apple Price is ${totalAmount} taka.`);
+// console.log(`Shopkepper returm me ${returnAmount} taka.`);
+
+// !JavaScript Variable Practice Task - 02! //
+// let mathematicsMark = 73;
+// let biologyMark = 27;
+// let chemistryMark = 67;
+// let physicsMark = 83;
+// let banglaMark = 59;
+// let englishMark = 97;
+
+// let totalMark = mathematicsMark + biologyMark + chemistryMark + physicsMark + banglaMark + englishMark;
+// let averageMark = totalMark / 6;
+// console.log(`Total Average Mark is ${parseFloat(averageMark.toFixed(2))}`);
+
+// !JavaScript Variable Practice Task - 03! //
+// let givenNumber = 119;
+// let rimainderNumber = givenNumber % 5;
+// console.log(rimainderNumber);
+
+// !JavaScript Variable Practice Task - 04! //
+//? isNan check the value is number or string. there is a = isNan ('11'). so isNaN convert this '11' string to number so isNaN show the answer is false. because of isNaN show answer false when the value is number and isNaN show answer true when the value is string, NaN, undefined.
+// var a = isNaN('11');
+// console.log(a);
+
+//? isNaN check the value is number or not. when the value is number isNaN show false and when the value is not a number then its show true. There is (2 - 10) = -8 is a valid number so isNaN show the answer false.
+// var a = isNaN(2 - 10);
+// console.log(a);
