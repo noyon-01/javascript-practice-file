7b2b2518c82a65a1b0212940812ac82aefac3d58
noyon-01 <shahadat.hosen.noyon.01@gmail.com>
2026-01-26 22:05:09 +0600

    javascript array practice task is added

--- javascript-array-task/array-task.js
+++ javascript-array-task/array-task.js
@@ -1,3 +1,4 @@
+// !JavaScript Array Methods Task - 01! //
 /**
  * Task-1
  - Declare an array
@@ -7,6 +8,13 @@
  -console log the final array
  */
 
+// let fruits = ["apple", "lemon", "mengo", "orange", "watermilon"];
+// console.log(fruits[3]);
+
+// fruits[2] = "Jambura";
+// console.log(fruits);
+
+// !JavaScript Array Methods Task - 02! //
 /**
  * Task-2
  - Add or remove elements
@@ -17,6 +25,13 @@
  - display the final array as output
  */
 
+// let place = ["Coxs Bazer", "Bandorban", "Kaptai Lake"];
+// place.push("Rangamati");
+// place.push("Sajak", "Baskali");
+// place.pop();
+// console.log(place);
+
+// !JavaScript Array Methods Task - 03! //
 /**
  * Task-3 
  - Checking Array Membership with ‘includes’
@@ -26,6 +41,11 @@
  - Print a message to the console indicating whether the element is present in the array or not.
  */
 
+//  let books = ['bangla', 'english', 'math', 'economics', 'physice'];
+//  let checkBook = books.includes('javascript');
+//  console.log(checkBook, "- javascript book are not available in the books containers");
+
+// !JavaScript Array Methods Task - 04! //
 /**
  * Task-4 
  - Checking if it's an Array
@@ -35,6 +55,15 @@
  - Print a message to the console indicating whether each variable is an array or not.
  */
 
+// let colors = ["red", "green", "yellow"];
+// let myName = "Shahadat Hosen Noyon";
+// let numbers = [1, 3, 4, 7, 9];
+
+// console.log(Array.isArray(colors));
+// console.log(Array.isArray(myName));
+// console.log(Array.isArray(numbers));
+
+// !JavaScript Array Methods Task - 05! //
 /**
  * Task-5 
  - Combining Arrays
@@ -44,3 +73,12 @@
  - Use the concat method to combine the two arrays into a new array.
  - Print both the original arrays and the combined array using console.log().
  */
+
+// let fruits = ["apple", "orange", "lemon", "mengo", "banana", "coconut"];
+// let numbers = [22, 53, 63, 64, 96, 90];
+
+// console.log(fruits);
+// console.log(numbers);
+
+// const newMixArray = fruits.concat(numbers);
+// console.log(newMixArray);
