3f243ff52a687c44814d06732de0615d83526ac9
noyon-01 <shahadat.hosen.noyon.01@gmail.com>
2026-01-27 19:47:42 +0600

    javascript for loop is added

---
+++ javascript-loops-types/loops.js
@@ -0,0 +1,60 @@
+// !JavaScript For Of Loop Starts Here! //
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// for (const number of numbers) {
+//   console.log(number);
+// }
+
+// let fruits = ["apple", "lemon", "orange", "mengo", "coconut", "watermilon"];
+
+// for (const fruit of fruits) {
+//   console.log(fruit);
+// }
+
+// !JavaScript For Loop Starts Here! //
+// for (let i = 0; i <= 10; i++) {
+//   console.log(i);
+// }
+
+// for (let num = 1; num <= 20; num = num + 2) {
+//   console.log(num);
+// }
+
+// for (let x = 1; x <= 20; x += 3) {
+//   console.log(x);
+// }
+
+// let sum = 0;
+// for (let i = 11; i <= 20; i++) {
+//   console.log(i);
+//   sum = sum + i;
+// }
+// console.log("Total of Sum is : ", sum);
+
+// for (let num = 1; num <= 20; num++) {
+//   if (num % 2 === 0) {
+//     console.log(num);
+//   }
+// }
+
+// for (let i = 10; i <= 30; i++) {
+//   if (i % 2 === 1) {
+//     console.log(i);
+//   }
+// }
+
+// for (let x = 10; x <= 30; x++) {
+//   if (x % 2 === 0) {
+//     console.log("Even number is : ", x);
+//   } else {
+//     console.log("Odd number is : ", x);
+//   }
+// }
+
+// !JavaScript While Loop Starts Here! //
+
+// !JavaScript Do While Loop Starts Here! //
+
+// !JavaScript Break of Loop Starts Here! //
+
+// !JavaScript Continue of Loop Starts Here! //
