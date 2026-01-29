a3a59cea9189e67740186134df172b94763b63db
noyon-01 <shahadat.hosen.noyon.01@gmail.com>
2026-01-29 22:44:24 +0600

    javascript loop break task is added

---
+++ javascript-loop-task/loop-task.js
@@ -0,0 +1,44 @@
+// !JavaScript Loop Break Task - 01 Start Here! //
+/* 
+Write a loop 1 to 200. Use break to exit the loop once you find 100.
+*/
+
+// for (let i = 1; i <= 200; i++) {
+//   if (i === 100) {
+//     break;
+//   }
+//   console.log(i);
+// }
+
+// !JavaScript Loop Break Task - 02 Start Here! //
+/* 
+Write a while loop that adds numbers starting from 1, but stops (using break) as soon as the sum reaches or exceeds 100
+*/
+
+// let number = 1;
+// let sum = 0;
+
+// while (number < 20) {
+//   if (sum >= 100) {
+//     break;
+//   }
+//   console.log(number)
+//   sum = sum + number;
+//   number++;
+// }
+
+// console.log("Total sum :", sum);
+
+// !JavaScript Loop Break Task - 03 Start Here! //
+/* 
+Write a loop that goes from 1 to 100, but stops (using break) when it encounters the first square number (like 4, 9, 16, etc.)
+*/
+
+// for (let i = 1; i < 100; i++) {
+//   if (i === 2 ** 2) {
+//     console.log("Found First Square Number :", i);
+//     break;
+//   }
+//   console.log(i);
+// }
+
