1da1541cf8127f637d002addf6f268b1fa42b268
noyon-01 <shahadat.hosen.noyon.01@gmail.com>
2026-01-29 23:08:02 +0600

    javascript for loop task is added

--- javascript-loop-task/loop-task.js
+++ javascript-loop-task/loop-task.js
@@ -67,3 +67,82 @@ display odd number from 55 to 85 and skip the numbers divisible by 5.
 //     console.log(i);
 //   }
 // }
+
+// !JavaScript For Loop Task - 01 Start Here! //
+/***
+"I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.
+*/
+
+// for (let i = 1; i <= 60; i++) {
+//   console.log(i, ":" ,"I will invest at least 6 hrs every single day for next 60 days!",);
+// }
+
+// !JavaScript For Loop Task - 02 Start Here! //
+/***
+ * Subtask-1:
+ * Find all the odd numbers from 61 to 100.
+ */
+
+// for (let i = 61; i <= 100; i++) {
+//   if (i % 2 !== 0) {
+//     console.log("Odd Numbers :", i);
+//   }
+// }
+
+/***
+ * Subtask-2:
+ * Find all the even numbers from 78 to 98.
+ */
+
+// for (let i = 78; i <= 98; i++) {
+//   if (i % 2 !== 1) {
+//     console.log("Even Numbers :", i);
+//   }
+// }
+
+// !JavaScript For Loop Task - 03 Start Here! //
+/***
+ * Subtask-1:
+ * Display sum of all the odd numbers from 91 to 129.
+ */
+
+// let sum = 0;
+// for (let i = 91; i <= 129; i++) {
+//   if (i % 2 === 1) {
+//     sum = sum + i;
+//   }
+// }
+// console.log(sum);
+
+/***
+ * Subtask-2:
+ * Display sum of all the even numbers from 51 to 85.
+ */
+
+// let sum = 0;
+// for (let i = 51; i <= 85; i++) {
+//   if (i % 2 === 0) {
+//     sum = sum + i;
+//   }
+// }
+// console.log(sum);
+
+// !JavaScript For Loop Task - 04 Start Here! //
+/***
+Generate a multiplication table for number 9
+*/
+
+// for (let num = 1; num <= 100; num++) {
+//   if (num % 9 === 0){
+//     console.log(num)
+//   }
+// }
+
+// !JavaScript For Loop Task - 05 Start Here! //
+/***
+Implement a countdown timer that counts down from 81 to 65.
+*/
+
+// for (let i = 81; i >= 65; i--) {
+//   console.log(i);
+// }
