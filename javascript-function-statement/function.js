c1ceef5f4216ad1a384d4c51c13c93bfdc5ebd7c
noyon-01 <shahadat.hosen.noyon.01@gmail.com>
2026-01-31 22:30:14 +0600

    javascript funtion parameter handle is added

--- javascript-function-statement/function.js
+++ javascript-function-statement/function.js
@@ -1,1 +1,35 @@
 // !JavaScript Function Syntex Starts Here! //
+// function myName() {
+//   console.log("My Name is : Shahadat Hosen Noyon");
+// }
+// myName();
+
+// function myNumber() {
+//   console.log("My Teletalk Mobile Number is : 01585735520");
+// }
+// myNumber();
+
+// !JavaScript Funtion Parameters Handle Starts Here! //
+// function addition(num1, num2) {
+//   const addition = num1 + num2;
+//   console.log(addition);
+// }
+// addition(5, 7);
+
+// function subtraction(num1, num2) {
+//   const subtraction = num1 - num2;
+//   console.log(subtraction);
+// }
+// subtraction(19, 4);
+
+// function multiplication(x) {
+//   const multiplication = x * x;
+//   console.log(multiplication);
+// }
+// multiplication(4);
+
+// function division(x, y) {
+//   const division = x / y;
+//   console.log(division);
+// }
+// division(125, 5);
