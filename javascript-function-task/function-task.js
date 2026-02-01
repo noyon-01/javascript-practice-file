ccc52e930a8702877816f3cfc29edc37f66a170b
noyon-01 <shahadat.hosen.noyon.01@gmail.com>
2026-02-01 14:56:33 +0600

    javascript funtion practice task 1, 2, 3 is added

---
+++ javascript-function-task/function-task.js
@@ -0,0 +1,66 @@
+// !JavaScript Function Practice Task - 01 Starts Here! //
+/**
+ * Task-1
+ * Take four parameters. Multiply the four numbers and then return the result
+ */
+
+// function multiply(num1, num2, num3, num4) {
+//   const result = num1 * num2 * num3 * num4;
+//   return result;
+// }
+// const result = multiply(2, 3, 4, 5);
+// console.log(result);
+
+// !JavaScript Function Practice Task - 02 Starts Here! //
+/**
+ * Task-2
+ * Take a number if the number is odd multiply it by 2 and return the result. If the number is even divide it by two and return the result.
+ */
+
+// function multiplyOrDivide(num) {
+//   if (num % 2 === 1) {
+//     const multiply = num * 2;
+//     return multiply;
+//   } else if (num % 2 === 0) {
+//     const divide = num / 2;
+//     return divide;
+//   }
+// }
+// const number1 = 5;
+// const multiplyResult = multiplyOrDivide(number1);
+// console.log("Multiply 2 of", number1, ":", multiplyResult);
+
+// const number2 = 8;
+// const divideResult = multiplyOrDivide(number2);
+// console.log("Divide 2 of", number2, ":", divideResult);
+
+// !JavaScript Function Practice Task - 03 Starts Here! //
+/**
+ * Task-3
+ * Write a function called make_avg() which will take an array of integers and the size of that array and return the average of those values.
+ */
+
+// function make_avg(integersArray) {
+//   const length = integersArray.length;
+//   let sum = 0;
+//   for (const array of integersArray) {
+//     sum = sum + array;
+//   }
+//   const result = sum / length;
+//   return result;
+// }
+// const integersArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+// const result = make_avg(integersArray);
+// console.log(result);
+
+// !JavaScript Function Practice Task - 04 Starts Here! //
+/**
+ * Task-4
+ * Write a function called count_zero() which will take a binary string (Binary string is a string which is consist of only 0 and 1) as parameter and count how many 0’s are there in that string.
+ */
+
+// !JavaScript Function Practice Task - 05 Starts Here! //
+/**
+ * Task-5
+ * Write a function called odd_even() which takes an integer value and tells whether this value is even or odd. If even return Even. If odd return Odd
+ */
