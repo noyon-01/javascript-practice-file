a0a7298c4c41012c80ed5f82d56ebed8ba0cc49f
noyon-01 <shahadat.hosen.noyon.01@gmail.com>
2026-02-02 16:01:57 +0600

    javascript math objects methods is added

---
+++ javascript-math-objects/math.js
@@ -0,0 +1,44 @@
+// !JavaScript Math.Min Function Starts Here! //
+// const minNum = Math.min(8, 3, 4, 5, 7, 1, 8, 9, 6);
+// console.log(minNum);
+
+// !JavaScript Math.Max Function Starts Here! //
+// const maxNum = Math.max(8, 3, 4, 5, 7, 1, 8, 9, 6);
+// console.log(maxNum);
+
+// !JavaScript Math.Abs Function Starts Here! //
+// const absNum = Math.abs(-7);
+// console.log(absNum);
+
+// !JavaScript Math.Round Function Starts Here! //
+// const roundNum1 = Math.round(3.85);
+// console.log(roundNum1);
+
+// const roundNum2 = Math.round(3.45);
+// console.log(roundNum2);
+
+// !JavaScript Math.Ceil Function Starts Here! //
+// const ceilNum = Math.ceil(5.23);
+// console.log(ceilNum);
+
+// !JavaScript Math.Floor Function Starts Here! //
+// const floorNum = Math.floor(6.95);
+// console.log(floorNum);
+
+// !JavaScript Math.Random Function Starts Here! //
+// const randomNum1 = Math.random();
+// console.log(randomNum1);
+
+// const randomNum2 = Math.random() * 10;
+// console.log(randomNum2);
+
+// const randomNum3 = Math.random() * 100;
+// const roundNumber = Math.round(randomNum3);
+// console.log(roundNumber);
+
+// let min = 10;
+// let max = 20;
+
+// const randomNum4 = Math.random() * (max - min + 1);
+// const roundNumber = Math.round(randomNum4) + min;
+// console.log(roundNumber);
