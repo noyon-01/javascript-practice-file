ddbe1934781900704cfffd5b1f57c3aa331143fb
noyon-01 <shahadat.hosen.noyon.01@gmail.com>
2026-02-05 14:48:25 +0600

    javascript simple problem task 7 is added

--- javascript-simple-problem-task/problem-task.js
+++ javascript-simple-problem-task/problem-task.js
@@ -126,8 +126,21 @@
 /**
  * Task-7:
  * Find the friend with the smallest name.
- * const heights2 = ['rahim', 'robin', 'rafi', 'ron', 'rashed'];
+ * const friends = ['rahim', 'robin', 'rafi', 'ron', 'rashed'];
  */
+
+// function smallestFriend(friends) {
+//   let tinyFriend = friends[0];
+//   for (const friend of friends) {
+//     if (friend.length < tinyFriend.length) {
+//       tinyFriend = friend;
+//     }
+//   }
+//   return tinyFriend;
+// }
+// const friends = ["rahim", "robin", "rafi", "ron", "rashed"];
+// const result = smallestFriend(friends);
+// console.log(result);
 
 // !JavaScript Simple Problem Practice Task - 08! //
 /**
