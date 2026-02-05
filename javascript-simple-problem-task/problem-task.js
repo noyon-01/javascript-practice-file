1fcc3299c6ac5c2e7162744068f0907a2f62f040
noyon-01 <shahadat.hosen.noyon.01@gmail.com>
2026-02-05 14:36:19 +0600

    javascript simple problem task 6 is added

--- javascript-simple-problem-task/problem-task.js
+++ javascript-simple-problem-task/problem-task.js
@@ -101,3 +101,70 @@
 // const randomNumber = Math.random() * (max - min + 1);
 // const roundNumber = Math.round(randomNumber) + min;
 // console.log(roundNumber);
+
+// !JavaScript Simple Problem Practice Task - 06! //
+/**
+ * Task-6:
+ * Find the lowest number in the array below.
+ * const heights = [167, 190, 120, 165, 137];
+ */
+
+// function lowestNumber(heights) {
+//   let lowestNum = heights[0];
+//   for (const height of heights) {
+//     if (height < lowestNum) {
+//       lowestNum = height;
+//     }
+//   }
+//   return lowestNum;
+// }
+// const heights = [167, 190, 120, 165, 137];
+// const result = lowestNumber(heights);
+// console.log(result);
+
+// !JavaScript Simple Problem Practice Task - 07! //
+/**
+ * Task-7:
+ * Find the friend with the smallest name.
+ * const heights2 = ['rahim', 'robin', 'rafi', 'ron', 'rashed'];
+ */
+
+// !JavaScript Simple Problem Practice Task - 08! //
+/**
+ * Task-8:
+ * Your task is to calculate the total budget required to buy electronics:
+   laptop = 35000 tk
+   tablet = 15000 tk
+   mobile = 20000 tk
+ * Write a JavaScript function named calculateElectronicsBudget that takes in the number of laptop, tablets, and mobile and returns the total money required.
+ */
+
+// !JavaScript Simple Problem Practice Task - 09! //
+/**
+ * Task-9:
+ * You are given an array of phone objects, each containing information about the model, brand, and price. Your task is to write a JavaScript function named findAveragePhonePrice that takes this array as input and returns the average price of phone.
+ * 
+ * Input
+ * const phones = [
+        { model: "PhoneA", brand: "Iphone", price: 95000 },
+        { model: "PhoneB", brand: "Samsung", price: 40000 },
+        { model: "PhoneC", brand: "Oppo", price: 26000 },
+        { model: "PhoneD", brand: "Nokia", price: 35000 },
+        { model: "PhoneE", brand: "Iphone", price: 105000 },
+        { model: "PhoneF", brand: "HTC", price: 48000 },
+    ];
+ */
+
+// !JavaScript Simple Problem Practice Task - 10! //
+/**
+ * Task-10: (Hard)
+ * For each employee their current salary is calculated by multiplying yearly increment with experience then adding the result to the starting salary. Now calculate is the total salary has to be provided by the company in a month.
+ * 
+ * Input
+ * const employees = [
+            { name: "shahin", experience: 5, starting: 20000, increment: 5000 },
+            { name: "shihab", experience: 3, starting: 15000, increment: 7000 },
+            { name: "shikot", experience: 9, starting: 30000, increment: 1000 },
+            { name: "shohel", experience: 0, starting: 29000, increment: 4000 },
+        ];
+ */
