f092109e41e66d2b297655a73bdf5a396cf0768b
noyon-01 <shahadat.hosen.noyon.01@gmail.com>
2026-02-27 22:27:28 +0600

    .

--- javascript-conditional-statement/condition.js
+++ javascript-conditional-statement/condition.js
@@ -127,3 +127,157 @@
 
 // const money = true;
 // !!money == false ? console.log ("You can entry this car") : console.log ("You can not entry this car");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// !JavaScript Simple Conditional Statement! //
+// let number = 30;
+// if (number > 20) {
+//   console.log("I am bigger gratter then 20");
+// } else {
+//   console.log("I am small less then 20");
+// }
+
+// const age = 18;
+// if (age >= 18) {
+//   console.log("You can drive this car");
+// } else {
+//   console.log("You can not drive this car");
+// }
+
+// let x = 10;
+// let y = 20;
+// if (x > y) {
+//   console.log("x is bigger then y");
+// } else {
+//   console.log("y is bigger then x");
+// }
+
+// const noyonAge = 20;
+// const nihanAge = 22;
+// if (noyonAge > nihanAge) {
+//   console.log("Noyon is younger brother");
+// } else {
+//   console.log("Nihal is younger brother");
+// }
+
+// !JavaScript Advanch Conditional Statement! //
+// let price = 5000;
+// if (price >= 5000) {
+//   let discountPrice = (price * 10) / 100; // !10% discount price!
+//   let payAmount = price - discountPrice;
+//   console.log(payAmount);
+// } else {
+//   console.log(price);
+// }
+
+// const age = 63;
+// const price = 999;
+// if (age <= 12) {
+//   console.log("You can eat free");
+// } else if (age >= 60) {
+//   const discountPrice = (price * 40) / 100; // !40% discount price!
+//   const payAmount = price - discountPrice;
+//   console.log(`You must pay ${parseInt(payAmount)} taks`);
+// } else {
+//   console.log(`You must pay ${price} taka`);
+// }
+
+// let price = 500;
+// const isLeader = true;
+// if (isLeader == false) {
+//   price = 0;
+// } else {
+//   price = price + 100;
+// }
+// console.log(price);
+
+// const poketMoney = 500;
+// if (poketMoney >= 1000) {
+//   console.log("Your are my rich friend");
+// } else if (poketMoney >= 500) {
+//   console.log("You are my poor friend");
+// } else {
+//   console.log("You are not my friend");
+// }
+
+// !JavaScript Nested Conditional Statement! //
+// let salary = 35000;
+// let height = 65;
+// if (salary >= 30000) {
+//   if (height >= 65) {
+//     console.log("You are a great man for merrage");
+//   } else {
+//     console.log("You are not feet for merrage");
+//   }
+// } else {
+//   console.log("You are not ready for merrage");
+// }
+
+// !JavaScript Logical and (&&), Logical or (||) and Logical not (!) Conditonal Statement! //
+// let salary = 10000;
+// let BCS = false;
+// if (salary >= 30000 && BCS === true) {
+//   console.log("You are selected for merrage");
+// } else if (salary >= 30000 || BCS === true) {
+//   console.log("You are not selected for merrage");
+// } else {
+//   console.log("You are a bad boy and your feature life is Dark");
+// }
+
+// const age = 18;
+// const money = true;
+// if (age >= 18 && money === true) {
+//   console.log("You can entry this Park and buy any product");
+// } else if (age >= 18 || money === true) {
+//   console.log("You can just entry this park");
+// } else {
+//   console.log("Dor Sala Gora Jai Gom Ja");
+// }
+
+// !JavaScript Ternary Operator! //
+// let price = 100;
+// let payAmount = 90;
+// payAmount >= price ? console.log("You have eat Briyani") : console.log("Yon can not eat now");
+
+// const age = 25;
+// age <= 30 ? console.log("You are a young man") : console.log ("You are a younger man");
+
+// !JavaScript Logical and (&&) Ternary Operator! //
+// const age = 15;
+// const money = true;
+// age >= 18 && money == true ? console.log ("You can entry this car") : console.log ("You can not entry this car");
+
+// !JavaScript Logical or (||) Ternary Operator! //
+// const age = 15;
+// const money = true;
+// age >= 18 || money == true ? console.log ("You can entry this car") : console.log ("You can not entry this car");
+
+// !JavaScript Logical not (!) Ternary Operator! //
+// const money = true;
+// !money == true ? console.log ("You can entry this car") : console.log ("You can not entry this car");
+
+// const money = true;
+// !!money == false ? console.log ("You can entry this car") : console.log ("You can not entry this car");
