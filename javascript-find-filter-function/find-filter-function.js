92176580227c81369554bb7d2495123963f68db3
noyon-01 <shahadat.hosen.noyon.01@gmail.com>
2026-02-28 23:00:46 +0600

    javascript find and filter methods is added

---
+++ javascript-find-filter-function/find-filter-function.js
@@ -0,0 +1,26 @@
+// !JavaScript find() Method Starts Here! //
+// let numbers = [10, 20, 25, 30, 40];
+// let result = numbers.find(num => num > 20);
+// console.log(result);
+
+// let users = [
+//   { id: 1, name: "Rahim" },
+//   { id: 2, name: "Karim" }
+// ];
+
+// let user = users.find(u => u.id === 2);
+// console.log(user);
+
+// !JavaScript filter() Method Starts Here! //
+// let numbers = [10, 20, 30, 40];
+// let result = numbers.filter((num) => num > 20);
+// console.log(result);
+
+// let users = [
+//   { id: 1, name: "Rahim" },
+//   { id: 2, name: "Karim" },
+//   { id: 3, name: "Rahim" }
+// ];
+
+// let result = users.filter(u => u.name === "Rahim");
+// console.log(result);
