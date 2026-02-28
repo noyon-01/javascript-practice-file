c6d84ef056870efc35f91c04a8b5bacbeeca9feb
noyon-01 <shahadat.hosen.noyon.01@gmail.com>
2026-02-28 15:09:54 +0600

    javascript undefined, null and nan is added

---
+++ javascript-null-nan-undefind-value/null-nan-undefind-value.js
@@ -0,0 +1,48 @@
+// ! JavaScript Undefined Starts Here !//
+// undefined কী?
+// undefined মানে — value এখনো দেওয়া হয়নি
+
+// let name;
+// console.log(name);
+
+// আমরা variable declare করেছি
+// কিন্তু value দিইনি
+// তাই JavaScript নিজে থেকে undefined দিয়েছে
+
+// function test() {}
+// console.log(test());
+
+// function কিছু return না করলে default undefined দেয়
+
+// ! JavaScript Null Starts Here !//
+// null কী?
+// null মানে — ইচ্ছা করে empty রাখা হয়েছে
+
+// let user = null;
+// console.log(user);
+
+// ! JavaScript NaN Starts Here !//
+// NaN কী?
+// NaN মানে — Not a Number
+// যখন math করতে গিয়ে number পাওয়া যায় না।
+
+// let result = "hello" / 2;
+// console.log(result);
+
+// "hello" কে number এ convert করা যায় না
+// তাই result = NaN
+
+// let x = Number("abc");
+// console.log(x);
+
+// একটা মজার বিষয়
+// console.log(typeof NaN);
+
+//! JavaScript Undefined, Null & NaN All In One Example !//
+// let salary;
+// let bonus = null;
+// let total = "abc" * 5;
+
+// console.log(salary);
+// console.log(bonus);
+// console.log(total);
