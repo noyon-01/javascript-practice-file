9b137686b112c2395ff75801fc45697a5af77b7d
noyon-01 <shahadat.hosen.noyon.01@gmail.com>
2026-02-28 22:42:43 +0600

    javascript destructuring of array and object is added

---
+++ javascript-array-object-destructuring/array-object-destructuring.js
@@ -0,0 +1,66 @@
+// !Array Destructuring Starts Here! //
+// let numbers = [10, 20, 30];
+// let first = numbers[0];
+// let second = numbers[1];
+// console.log(first);
+// console.log(second);
+
+// let numbers = [10, 20, 30];
+// let [first, second] = numbers;
+// console.log(first);
+// console.log(second);
+
+// let numbers = [10, 20, 30];
+// let [first, , third] = numbers;
+// console.log(first);
+// console.log(third);
+
+// let numbers = [10];
+// let [first, second = 50] = numbers;
+// console.log(first);
+// console.log(second);
+
+// !Object Destructuring Starts Here! //
+// let person = {
+//   name: "Rahim",
+//   age: 25,
+// };
+
+// let name = person.name;
+// let age = person.age;
+
+// console.log(name);
+// console.log(age);
+
+// let person = {
+//   name: "Rahim",
+//   age: 25
+// };
+// let { name, age } = person;
+
+// console.log(name);
+// console.log(age);
+
+// let person = {
+//   name: "Rahim",
+//   age: 25
+// };
+// let { name: userName } = person;
+// console.log(userName);
+
+// let person = {
+//   name: "Rahim",
+// };
+// let { name, age = 18 } = person;
+// console.log(age);
+
+// function greet({ name, age }) {
+//   console.log("Name:", name);
+//   console.log("Age:", age);
+// }
+
+// let person = {
+//   name: "Karim",
+//   age: 30
+// };
+// greet(person);
