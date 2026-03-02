179f46d5db503d9e75588aed9647fbe384617746
noyon-01 <shahadat.hosen.noyon.01@gmail.com>
2026-03-02 23:32:08 +0600

    javascript async and await method is added

--- javascript-api-calling/api.js
+++ javascript-api-calling/api.js
@@ -1,4 +1,5 @@
 // !JavaScript Default Api Calling Starts Here!//
+// ?Post Api Calling?//
 // const load_data = () => {
 //   fetch("https://jsonplaceholder.typicode.com/posts")
 //     .then((res) => res.json())
@@ -20,6 +21,7 @@
 // };
 
 //!-----------------------------------------------!//
+// ?Comments Api Calling?//
 // const load_data = () => {
 //   const url = "https://jsonplaceholder.typicode.com/comments";
 //   fetch(url)
@@ -44,4 +46,57 @@
 //     comment_container.appendChild(div);
 //   });
 // };
+
+// !----------------------------------------------!//
+
+// ?User Api Calling?//
+// const load_data = () => {
+//   const url = "https://jsonplaceholder.typicode.com/users";
+//   fetch(url)
+//     .then((res) => res.json())
+//     .then((data) => display_data(data));
+// };
+
+// const display_data = (users) => {
+//   const user_container = document.getElementById("user_container");
+//   user_container.innerHTML = "";
+
+//   users.forEach((user) => {
+//     const div = document.createElement("div");
+//     div.innerHTML = `
+//     <div class="users_section">
+//     <h2 style="font-weight: 700;">Name: ${user.name}</h2>
+//     <p style="margin-top: 10px; font-weight: 600;">Email: ${user.email}</p>
+//     <p style="margin-top: 10px; font-weight: 600;">Street Address: ${user.address.street}</p>
+//     <p style="margin-top: 10px; font-weight: 600;">City Address: ${user.address.city}</p>
+//     <p style="margin-top: 10px; font-weight: 600;">Zipcode: ${user.address.zipcode}</p>
+//     <p style="margin-top: 10px; font-weight: 600;">Phone: ${user.phone}</p>
+//     <p style="margin-top: 10px; font-weight: 600;">Website: ${user.website}</p>
+//     <p style="margin-top: 10px; font-weight: 600;">Company Name: ${user.company.name}</p>
+//     </div>`;
+
+//     user_container.appendChild(div);
+//   });
+// };
+
 // !JavaScript Async Await Api Calling Starts Here!//
+// ?The below code problem is "I am Printing after Loading Todos is Print After Todos"? //
+// const load_data = () => {
+//   console.log("I am Printing before Loading Todos.");
+//   const url = "https://jsonplaceholder.typicode.com/todos";
+//   fetch(url)
+//     .then((res) => res.json())
+//     .then((data) => console.log(data));
+//   console.log("I am Printing after Loading Todos.");
+// };
+
+// ?This Problem Solving Using Async and Await Methods? //
+// const load_data = async () => {
+//   console.log("I am Printing before Loading Todos.");
+
+//   const resp = await fetch("https://jsonplaceholder.typicode.com/todos");
+//   const data = await resp.json();
+//   console.log(data);
+
+//   console.log("I am Printing after Loading Todos.");
+// };
